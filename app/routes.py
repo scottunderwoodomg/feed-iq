e46cb02c9b7a90b99d726c6c11a2bd857cc02640
Scott Underwood <37125095+scottunderwoodomg@users.noreply.github.com>
2022-05-30 09:52:13 -0400

    basic login functionality

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,39 @@
 from flask import render_template
+from flask import flash
+from flask import redirect
+from flask import url_for
 from app import app
+from app.forms import LoginForm
 from datetime import datetime
 
-@app.route('/')
-@app.route('/index')
+
+@app.route("/")
+@app.route("/index")
 def index():
-    user = {'username': 'Scott'}
+    user = {"username": "Scott"}
     feeds = [
         {
-            'child': 'Charlie',
-            'feed_datetime': datetime(2022,5,29,11,46,42),
-            'feed_type': 'Breast'
+            "child": "Charlie",
+            "feed_datetime": datetime(2022, 5, 29, 11, 46, 42),
+            "feed_type": "Breast",
         },
         {
-            'child': 'Charlie',
-            'feed_datetime': datetime(2022,5,29,8,35,41),
-            'feed_type': 'Breast'
-        }
+            "child": "Charlie",
+            "feed_datetime": datetime(2022, 5, 29, 8, 35, 41),
+            "feed_type": "Breast",
+        },
     ]
-    return render_template('index.html', title='Home', user=user, feeds=feeds)
+    return render_template("index.html", title="Home", user=user, feeds=feeds)
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            "Login requested for user {}, remember_me={}".format(
+                form.username.data, form.remember_me.data
+            )
+        )
+        return redirect(url_for("index"))
+    return render_template("login.html", title="Sign In", form=form)
