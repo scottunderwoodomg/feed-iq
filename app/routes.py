018f937e18b1b4a363c2ebfc25b936b529c175ee
Scott Underwood <37125095+scottunderwoodomg@users.noreply.github.com>
2022-05-30 06:52:30 -0400

    establish html templating

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
+from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hey, welcome to FeedIQ!"
+    user = {'username': 'Scott'}
+    feeds = [
+        {
+            'child': 'Charlie',
+            'feed_datetime': datetime(2022,5,29,11,46,42),
+            'feed_type': 'Breast'
+        },
+        {
+            'child': 'Charlie',
+            'feed_datetime': datetime(2022,5,29,8,35,41),
+            'feed_type': 'Breast'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, feeds=feeds)
+
